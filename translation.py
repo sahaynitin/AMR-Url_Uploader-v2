c6685364d0a64296d2b88e844f705e1a0e5a8796
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-27 14:52:49 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -61,7 +61,7 @@ class Translation(object):
         InlineKeyboardButton('⛔ Close', callback_data='close')
         ]]
     )
-    FORMAT_SELECTION = """<b>Select The Desire Format Below</b>"""
+    FORMAT_SELECTION = """<b>Select The Desire Format For Uploading In File Or Video</b>"""
     CHECKING_LINK = "<code>Analysing Your Link</code>⏳"
     BANNED_USER_TEXT = "<code>You are Banned!</code>"
     SET_CUSTOM_USERNAME_PASSWORD = """"""
